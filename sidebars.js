43ffda995d6f672139316cd18454b219b22361df
SAY-droid427 <sayanimallick0218@gmail.com>
2021-05-01 15:13:30 +0530

    Added article for moore's voting algorithm

--- sidebars.js
+++ sidebars.js
@@ -154,6 +154,7 @@ module.exports = {
                 "competitive-programming/factorial-of-large-number",
                 "competitive-programming/max-number-from-array",
                 "competitive-programming/reverse-of-string",
+                "competitive-programming/moore-voting-algorithm"
             ],
         },
         {
